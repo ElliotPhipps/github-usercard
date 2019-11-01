f5c058756626f7cab8cad1af0349e253b6c72479
Elliot Phipps <phipps.elliot@gmail.com>
2019-11-01 10:59:54 -0400

    Finished

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,18 +2,20 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
-axios.get("https://api.github.com/users/ElliotPhipps");
-  .then(response => {
-  // console.log(response.data.message);
-  response.data.message.forEach(item => {
-    const newDog = DogCard(item)
-    entryPoint.appendChild(newDog)
-  });
-})
-.catch(error => {
-  console.log("The data was not returned", error)
-})
+axios.get("https://api.github.com/users/ElliotPhipps")
+
+.then(response => {
+
+  //console.log(response);
+
+  const cards = document.querySelector('.cards');
+
+  cards.appendChild(gitUserCard(response.data));
+
+});
+
 const entryPoint = document.querySelector(".cards");
+
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
@@ -35,7 +37,35 @@ const entryPoint = document.querySelector(".cards");
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = [
+
+  `https://api.github.com/users/crutledgedev`,
+
+  `https://api.github.com/users/EricFerguson76`,
+
+  `https://api.github.com/users/lyndsiWilliams`,
+
+  `https://api.github.com/users/AJWD92`,
+
+  `https://api.github.com/users/squashgray`,
+
+];
+
+followersArray.forEach(link =>{
+
+  axios.get(link)
+
+  .then(response => {
+
+   //console.log(response);
+
+   const cards = document.querySelector('.cards');
+
+   cards.appendChild(gitUserCard(response.data));
+
+  });
+
+});
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -57,10 +87,11 @@ const followersArray = [];
 
 */
 
-function createCard(card) {
+function gitUserCard(object) {
+
   // define new elements
 
-  const card1 = document.createElement('div');
+  const card = document.createElement('div');
 
   const image = document.createElement('img');
 
@@ -74,17 +105,41 @@ function createCard(card) {
 
   const profiles = document.createElement('p');
 
+  const gitAddress = document.createElement('a');
+
   const followers = document.createElement('p');
 
   const following = document.createElement('p');
 
   const bios = document.createElement('p');
 
+  //set content
+
+  image.src = object.avatar_url;
+
+  names.textContent = object.name;
+
+  usernames.textContent = object.login;
+
+  locations.textContent = `Location: ${object.location}`;
+
+  profiles.textContent = `Profile: `;
+
+  gitAddress.textContent = object.html_url;
+
+  gitAddress.href = object.html_url;
+
+  followers.textContent = `Followers: ${object.followers}`;
+
+  following.textContent = `Following: ${object.following}`;
+
+  bios.textContent = `Bio: ${object.bio}`;
+
   // setup structure of elements
 
-  card1.appendChild(image);
+  card.appendChild(image);
 
-  card1.appendChild(cardInfo);
+  card.appendChild(cardInfo);
 
   cardInfo.appendChild(names);
 
@@ -94,6 +149,8 @@ function createCard(card) {
 
   cardInfo.appendChild(profiles);
 
+  profiles.appendChild(gitAddress);
+
   cardInfo.appendChild(followers);
 
   cardInfo.appendChild(following);
@@ -101,20 +158,20 @@ function createCard(card) {
   cardInfo.appendChild(bios);
 
   // setup class names
-  card1.classList.add('card')
+
+  card.classList.add('card')
 
   cardInfo.classList.add('card-info')
 
-  name.classList.add(name)
+  names.classList.add('name')
 
-  username.classList.add('username')
+  usernames.classList.add('username')
 
   // return card
-
+  
   return card
 
 }
-
 
 /* List of LS Instructors Github username's: 
   tetondan
